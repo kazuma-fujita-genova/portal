fe8418e5afd17c5e30b36ee8dc9645e5fbc5c574
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2018-10-16 10:08:32 +0900

    Initial Commit

---
+++ portal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  portal
+//
+//  Created by 藤田和磨 on 2018/10/16.
+//  Copyright © 2018 藤田和磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
