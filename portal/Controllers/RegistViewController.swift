01ee83765da9782d0a188026532e48c7641ab9f1
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2018-10-23 19:19:29 +0900

    新規登録のプレースホルダーを正常動作させるよう修正 / プロフィールのテーブル部品雛形作成

--- portal/Controllers/RegistViewController.swift
+++ portal/Controllers/RegistViewController.swift
@@ -18,35 +18,47 @@ class RegistViewController: UIViewController {
     
     @IBOutlet weak var sendButton: UIButton!
     
-    
     @IBAction func handleSendButton(_ sender: Any) {
     }
     
+    var allTextFieldControllers = [MDCTextInputControllerUnderline]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setupTextFields()
+        // addGestureRecognizer()
     }
     
     func setupTextFields() {
         let nicknameFieldController = MDCTextInputControllerUnderline(textInput: nicknameField)
-        nicknameField.delegate = self as? UITextFieldDelegate
-        //nicknameField.placeholder = "Name"
+        // nicknameField.delegate = self
         nicknameField.autocapitalizationType = .words
         nicknameFieldController.placeholderText = "ニックネーム"
         nicknameFieldController.helperText = "10文字以内"
-        let nicknameTextInput = nicknameFieldController.textInput as! MDCTextField
-        nicknameTextInput.tag = 0
+        // let nicknameTextInput = nicknameFieldController.textInput as! MDCTextField
+        // nicknameTextInput.tag = 0
+        self.allTextFieldControllers.append(nicknameFieldController)
         
         let mobileTelFieldController = MDCTextInputControllerUnderline(textInput: mobileTelField)
-        mobileTelField.delegate = self as? UITextFieldDelegate
+        // mobileTelField.delegate = self as? UITextFieldDelegate
         mobileTelFieldController.placeholderText = "携帯電話番号"
         mobileTelFieldController.helperText = "ハイフンなし"
-        let mobileTelTextInput = mobileTelFieldController.textInput as! MDCTextField
-        mobileTelTextInput.tag = 1
-        
+        // let mobileTelTextInput = mobileTelFieldController.textInput as! MDCTextField
+        // mobileTelTextInput.tag = 1
+        self.allTextFieldControllers.append(mobileTelFieldController)
+    }
+    
+    func addGestureRecognizer() {
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapDidTouch(sender: )))
+        self.view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    
+    // MARK: - Actions
+    @objc func tapDidTouch(sender: Any) {
+        self.view.endEditing(true)
     }
     /*
     // MARK: - Navigation
@@ -57,4 +69,10 @@ class RegistViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
+
+extension RegistViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
